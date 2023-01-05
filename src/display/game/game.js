4030a67946db2834a7b151cd35dc205a8c547244
Sean Wallace <wallace.sean111@gmail.com>
2023-01-05 12:32:54 +0100

    Remove names and quotes from cards

--- src/display/game/game.js
+++ src/display/game/game.js
@@ -8,9 +8,6 @@ const Game = (props) => {
         return (
         <div className="card" key={card.id} data-id={card.id} onClick={props.clickCard}>
           <img src={card.image} alt="A black tar-like pool in humanoid form" data-id={card.id}/>
-          <h5 className="card-name" data-id={card.id}>{card.name}</h5>
-          <h6 className="quote" data-id={card.id}>{card.quote}</h6>
-
         </div>
         )
         
