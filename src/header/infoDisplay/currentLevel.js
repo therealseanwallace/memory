2f6fad2e1dadd779d025254510ce14ffcf81a08f
Sean Wallace <wallace.sean111@gmail.com>
2023-01-05 17:46:55 +0100

    Clean code

--- src/header/infoDisplay/currentLevel.js
+++ src/header/infoDisplay/currentLevel.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const CurrentLevel = (props) => {
   console.log("CurrentLevel rendered. props are: ", props);
-  const test = "test";
   return (
     <div>
       <h4 className={"current-level-title stats-title"}>
